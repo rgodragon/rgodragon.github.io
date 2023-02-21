2f220120e9c513ef62baf92430d6e00f47e820ed
rgodragon <rgodragon@gmail.com>
2023-02-21 14:11:40 +0900

    change theme

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -384,7 +384,7 @@ var store = [{
         "teaser": "https://farm5.staticflickr.com/4076/4940499208_b79b77fb0a_z.jpg"
       },{
         "title": "Layout: Header Video",
-        "excerpt":"This post should display a header with a responsive video, if the theme supports it. Although audiobooks are increasingly being used, people tend to perceive audiobook experiences as ’not real reading’ due to its in- tangibility and ephemerality. In this paper, we developed ADIO, a device augmenting audiobook experience through...","categories": ["Layout","Uncategorized"],
+        "excerpt":"This post should display a header with a responsive video, if the theme supports it. Audiobook-Shelf (for Naver AudioClip) Although audiobooks are increasingly being used, people tend to perceive audiobook experiences as ’not real reading’ due to its in- tangibility and ephemerality. In this paper, we developed ADIO, a device...","categories": ["Layout","Uncategorized"],
         "tags": ["video","layout"],
         "url": "/layout/uncategorized/layout-header-video/",
         "teaser": null
--- cv/_vendor/bundle/ruby/3.2.0/gems/ffi-1.15.4.pre1/lib/ffi/version.rb
+++
@@ -1,3 +0,0 @@
-module FFI
-  VERSION = '1.15.4.pre1'
-end
